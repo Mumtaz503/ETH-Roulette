530d8ed8975dbbdad8da6b7342d89b6588ff6aa4
Mumtaz503 <132309236+Mumtaz503@users.noreply.github.com>
2024-02-08 18:31:18 +0500

    Changed the import path for `AutomationCompatibleInterface.sol` in chainlink/contracts v0.8

--- contracts/Roulette.sol
+++ contracts/Roulette.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.7;
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
+import "@chainlink/contracts/src/v0.8/automation/interfaces/AutomationCompatibleInterface.sol";
 //import "hardhat/console.sol";
 
 /* Custom error codes */
